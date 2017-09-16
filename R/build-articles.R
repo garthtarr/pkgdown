f0b3aede7bda026d59413436f53cb8ec12f2bf7b
Garth Tarr <garth.tarr@gmail.com>
2017-09-16 16:02:29 +1000

    default toc_depth

--- R/build-articles.R
+++ R/build-articles.R
@@ -132,7 +132,7 @@ build_rmarkdown_format <- function(pkg = ".",
                                    depth = 1L,
                                    data = list(),
                                    toc = TRUE,
-                                   toc_depth = toc_depth) {
+                                   toc_depth = 2) {
   # Render vignette template to temporary file
   path <- tempfile(fileext = ".html")
   suppressMessages(
